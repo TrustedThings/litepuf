c3d271c1b4cb78a93a584bdf8729e38128cac7c0
Arnaud Durand <arnaud.durand@unifr.ch>
2020-01-06 17:01:25 +0100

    Use wb.regs directly

--- examples/litescope_client.py
+++ examples/litescope_client.py
@@ -1,6 +1,7 @@
 from itertools import product
 from sys import stdout
 import json
+import ctypes
 
 from litex.tools.litex_client import RemoteClient
 from litescope.software.driver.analyzer import LiteScopeAnalyzerDriver
@@ -18,26 +19,15 @@ analyzer.add_rising_edge_trigger("puf_reset")
 
 analyzer.run(offset=8, length=512)  ### CHANGE THIS TO MATCH DEPTH offset=32 by default
 
-with open("test/csr.json") as csr_file:
-    csr = json.load(csr_file)
-
-csr_registers = csr["csr_registers"]
-
-reset = csr_registers["teropuf_reset"]
-cell_select = (
-    csr_registers["teropuf_cell0_select"],
-    csr_registers["teropuf_cell1_select"]
-)
-puf_val = csr_registers["teropuf_bit_value"]
-
 for i, j in product(range(2), repeat=2):
-    wb.write(reset['addr'], 1) # enable reset
-    wb.write(cell_select[0]['addr'], i)
-    wb.write(cell_select[1]['addr'], j)
-    wb.write(reset['addr'], 0) # disable reset
+    wb.regs.teropuf_reset.write(1) # enable reset
+    wb.regs.teropuf_cell0_select.write(i)
+    wb.regs.teropuf_cell1_select.write(j)
+    wb.regs.teropuf_reset.write(0) # disable reset
     print(f'Comparator from set {i} and {j}:')
     for _ in range(10):
-        print(wb.read(puf_val['addr']))
+        print(wb.regs.teropuf_bit_value.read())
+        print(ctypes.c_int32(wb.regs.teropuf_bit_value.read()).value)
 
 analyzer.wait_done()
 analyzer.upload()
