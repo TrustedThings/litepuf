b455bcfbce557869058439b2f0ad70ea80b863ad
Arnaud Durand <arnaud.durand@unifr.ch>
2021-05-14 16:39:10 +0200

    Update TRNG testbench

--- examples/trng_remote.py
+++ examples/trng_remote.py
@@ -1,5 +1,6 @@
 from itertools import product
 from sys import stdout
+import time
 import json
 import ctypes
 
@@ -13,11 +14,14 @@ analyzer = LiteScopeAnalyzerDriver(wb.regs, "analyzer", debug=True, config_csv="
 
 analyzer.run(length=2**20)  ### CHANGE THIS TO MATCH DEPTH offset=32 by default
 
-with open('delete_me.dat', 'ab') as f:
+with open('entropy.dat', 'ab') as f:
     for i in range(10):
-        random_word16 = wb.regs.trng_random_word.read()
-        print(hex(random_word16))
-        f.write(random_word16.to_bytes(2, 'big'))
+        wb.regs.trng_update_value.write(1)
+        while not wb.regs.trng_ready.read():
+            pass
+        random_word = wb.regs.trng_random_word.read()
+        print(hex(random_word))
+        f.write(random_word.to_bytes(4, 'big'))
 
 analyzer.wait_done()
 analyzer.upload()
