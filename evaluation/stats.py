6ff54f39450f874e59bce4cbc4e3edd603ad5dbb
Arnaud Durand <arnaud.durand@unifr.ch>
2021-05-25 23:29:00 +0200

    Update evaluation

---
+++ evaluation/stats.py
@@ -0,0 +1,77 @@
+from itertools import permutations
+from operator import itemgetter
+import statistics
+
+
+def hamming_dist(x, y):
+    return bin(x ^ y).count('1')
+
+def hamming_weight(x):
+    return bin(x).count('1')
+
+def bitwise_mode(iterable, n):
+    iter_bin = [bin(x)[2:].zfill(n) for x in iterable]
+    modes = [statistics.mode(map(itemgetter(b), iter_bin)) for b in range(n)]
+    return int(''.join(modes), 2)
+
+def graycode(n):
+    return n ^ (n >> 1)
+
+def uniqueness(chip_dumps, response_len=1):
+    def permute_chips(chip_dumps, response_len):
+        for chip1, chip2 in permutations(chip_dumps, 2):
+            distances = list()
+            for challenge in chip1:
+                resp1 = bitwise_mode(chip1[challenge], response_len)
+                resp2 = bitwise_mode(chip2[challenge], response_len)
+                distances.append(hamming_dist(resp1, resp2))
+            yield statistics.mean(
+                [distance/response_len for distance in distances]
+            )  
+
+    k_chips = len(chip_dumps)
+    sum_permutations = sum(permute_chips(chip_dumps, response_len))
+    return 2 / (k_chips*(k_chips-1)) * sum_permutations
+
+# for one chip
+def steadiness(chip_dump, references, response_len=1):
+    for challenge, responses in chip_dump.items():
+        ref = references[challenge]
+        distances = [hamming_dist(resp, ref) for resp in responses]
+        # yield a value for each different challenge
+        yield 1 - statistics.mean(
+            [distance/response_len for distance in distances]
+        )
+
+def randomness():
+    pass
+
+
+import unittest
+
+
+class EvaluationTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.chips_cr = [
+            {
+                'challenge1': [0,0,0,0],
+                'challenge2': [0,0,0,0],
+            },
+            {
+                'challenge1': [1,1,1,0],
+                'challenge2': [0,0,0,0],
+            },    
+        ]
+
+    def test_uniqueness(self):
+        uniqueness_ = uniqueness(self.chips_cr, response_len=1)
+        print(uniqueness_)
+
+    def test_steadiness(self):
+        references = {
+            'challenge1': 1,
+            'challenge2': 0,
+        }
+        steadiness_ = steadiness(self.chips_cr[0], references, response_len=1)
+        print(list(steadiness_))
--- evaluation/teropuf.py
+++ evaluation/teropuf.py
@@ -2,15 +2,18 @@ from itertools import permutations
 import argparse
 from glob import glob
 import json
-import statistics
-from operator import sub
+from statistics import mode, mean
+from operator import sub, itemgetter
+from collections import defaultdict
 from itertools import starmap
 import ctypes
 
-from more_itertools import all_equal
-
 from .stats import steadiness, uniqueness, graycode
 
+import matplotlib.pyplot as plt
+
+    
+_get_bit = lambda n, i: n >> i & 1
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -21,26 +24,50 @@ if __name__ == "__main__":
     dump_files = list()
     for arg in args.dump_files:  
         dump_files += glob(arg)
-    print(dump_files)
 
     response_dumps = []
+    analyzer_dumps = []
     for filename in dump_files:
         with open(filename, 'r') as f:
             response_data = json.load(f)
+        response_dumps.append(response_data['dump'])
+        analyzer_dumps.append(response_data['analyzer'])
 
-        cell1 = response_data['teropuf_cell0_select_storage']
-        cell2 = response_data['teropuf_cell1_select_storage']
-        select1 = response_data['teropuf_roset0_select']
-        select2 = response_data['teropuf_roset1_select']
-        assert(all_equal(select1))
-        assert(all_equal(select2))
-        challenge = f'{select1[0]}:{select2[0]}'
+    chip_offset_cr_list = []
 
-        # each clock cycles has two values (rising, falling), skip every other value
-        counter1 = response_data['teropuf_roset0_counter'][::2] 
-        counter2 = response_data['teropuf_roset1_counter'][::2]
-        responses_sub = starmap(sub, zip(counter1, counter2))
-        responses = [ctypes.c_uint16(r).value for r in responses_sub]
-        responses_gray = map(graycode, responses)
+    for chip in analyzer_dumps:
+        steadiness_plot_data = defaultdict(list)
+        offset_cr_dict = dict()
+        chip_offset_cr_list.append(offset_cr_dict)
 
-        response_dumps.append(list(responses_gray))
+        for offset, cr in chip.items():
+            # challenge-responses for offset
+            offset_cr_dict[offset] = cr_dict = dict()
+
+            for challenge, responses in cr.items():
+                responses_gray = [ctypes.c_uint16(r).value for r in responses]
+                responses_gray = list(map(graycode, responses_gray))
+                cr_dict[challenge] = responses_gray
+
+            for bit_num in range(16):
+                cr_bit_dict = {c: list(map(lambda r: _get_bit(r, bit_num), responses)) for c, responses in cr_dict.items()}
+                references = {c: mode(responses) for c, responses in cr_bit_dict.items()}
+                steadiness_ = mean(steadiness(cr_bit_dict, references, response_len=1))
+
+                steadiness_plot_data[bit_num].append(steadiness_)
+
+        plt.plot(
+            chip.keys(), steadiness_plot_data[0], 'r--',
+            chip.keys(), steadiness_plot_data[1], 'g--',
+            chip.keys(), steadiness_plot_data[2], 'b--',
+            chip.keys(), steadiness_plot_data[13], 'r^',
+            chip.keys(), steadiness_plot_data[14], 'g^',
+            chip.keys(), steadiness_plot_data[15], 'b^',
+        )
+
+    for offset in analyzer_dumps[0]:
+        cr_dict = list(map(itemgetter(offset), chip_offset_cr_list))
+        for bit_num in range(16):
+            uniqueness_ = uniqueness(cr_dict, response_len=1)
+
+    plt.show()
