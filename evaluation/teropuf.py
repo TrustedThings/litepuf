fd40c1569d17f00953d7448c4e89b5702897da45
Arnaud Durand <arnaud.durand@unifr.ch>
2021-06-01 23:03:51 +0200

    Simplify dump analysis

--- evaluation/teropuf.py
+++ evaluation/teropuf.py
@@ -9,23 +9,10 @@ from collections import defaultdict
 from itertools import starmap
 import ctypes
 
-from .stats import steadiness, uniqueness, graycode
+from metastable.evaluation import steadiness, uniqueness, graycode
 
 import matplotlib.pyplot as plt
 
-@lru_cache(maxsize=128)
-def chip_post_iter(dumps, bit_slice=None):
-    # mask of response bits
-    slice_bits = lambda n: int(bin(n)[2:].zfill(16)[bit_slice], 2)
-    for chip in dumps:
-        chip_item = {}
-        for challenge, responses in chip.items():
-            responses_post = [ctypes.c_uint16(r).value for r in responses]
-            responses_post = map(graycode, responses_post)
-            if bit_slice:
-                responses_post = map(slice_bits, responses_post)
-            chip_item[challenge] = list(responses_post)
-        yield chip_item
 
 def postprocess_response(response, bit_slice=None):
     # mask of response bits
@@ -61,51 +48,62 @@ if __name__ == "__main__":
     uniqueness_plot_data = defaultdict(list)
 
     #offsets = analyzer_dumps[0].keys()
+    offset_dim = 'voltage'
 
-    offsets = [r['voltage'] for r in list(response_dumps[0].values())[0] if 'voltage' in r]
-    print(offsets)
+    offsets = [r[offset_dim] for r in list(response_dumps[0].values())[0] if offset_dim in r]
+    offsets = list(dict.fromkeys(offsets)) # remove duplicates and keep order (Python > 3.6) 
 
-    for bit_num in range(16):
+    slices = [None]
+    #slices = [slice(bit_num, bit_num+1) for bit_num in range(16)]
+
+    steadiness_plot_data = [list() for _ in range(len(slices))]
+    steadiness_err_data = [list() for _ in range(len(slices))]
+    uniqueness_plot_data = [list() for _ in range(len(slices))]
+
+    for slice_idx, bit_slice in enumerate(slices):
         for offset in offsets:
             #chips = list(chip_post_iter(analyzer_dumps, offset, slice(bit_num, bit_num+1)))
             chips = []
             for chip_dump in response_dumps:
                 chip = dict()
                 for c, responses in chip_dump.items():
-                    responses = list(filter(lambda r: r['voltage'] == offset, responses))
-                    responses = [postprocess_response(r['value'], slice(bit_num, bit_num+1)) for r in responses]
+                    responses = list(filter(lambda r: offset_dim in r and r[offset_dim] == offset, responses))
+                    #responses = [postprocess_response(r['value'], slice(bit_num, bit_num+1)) for r in responses]
+                    responses = [postprocess_response(r['value'], bit_slice) for r in responses]
                     chip[c] = responses
                 chips.append(chip)
 
-            #uniqueness_ = uniqueness(chips, response_len=1)
-            uniqueness_ = 0
-            uniqueness_plot_data[bit_num].append(uniqueness_)
+            uniqueness_ = uniqueness(chips, response_len=1)
+            uniqueness_plot_data[slice_idx].append(uniqueness_)
+
             for chip in chips:
                 references = {c: mode(responses) for c, responses in chip.items()}
                 steadiness_ = list(steadiness(chip, references, response_len=1))
                 steadiness_mean = mean(steadiness_)
-                steadiness_plot_data[bit_num].append(steadiness_mean)
-                steadiness_err = steadiness_mean-min(steadiness_), max(steadiness_)-steadiness_mean
-                steadiness_err_data[bit_num].append(steadiness_err)
                 # plot steadiness for one chip
+                steadiness_plot_data[slice_idx].append(steadiness_mean)
+                steadiness_err = steadiness_mean-min(steadiness_), max(steadiness_)-steadiness_mean
+                steadiness_err_data[slice_idx].append(steadiness_err)
                 break
 
     fig, (ax_steadiness, ax_uniqueness) = plt.subplots(2)
 
     yerr = [(
-        list(map(itemgetter(0), steadiness_err_data[bit_num])),
-        list(map(itemgetter(1), steadiness_err_data[bit_num]))
-    ) for bit_num in range(16)]
-    ax_steadiness.errorbar(offsets, steadiness_plot_data[0], yerr=yerr[0], fmt='yo-') # bit 15
-    ax_steadiness.errorbar(offsets, steadiness_plot_data[11], yerr=yerr[11], fmt='o-') # bit 4
-    ax_steadiness.errorbar(offsets, steadiness_plot_data[10], yerr=yerr[10], fmt='o-') # bit 5
-    ax_steadiness.errorbar(offsets, steadiness_plot_data[9], yerr=yerr[9], fmt='o-') # bit 6
+        list(map(itemgetter(0), steadiness_err_data[slice_idx])),
+        list(map(itemgetter(1), steadiness_err_data[slice_idx]))
+    ) for slice_idx in range(len(slices))]
+
+    ax_steadiness.errorbar(offsets, steadiness_plot_data[0], fmt='ro', linestyle='-') # bit 15
+    #ax_steadiness.errorbar(offsets, steadiness_plot_data[11], yerr=yerr[11], fmt='o-') # bit 4
+    #ax_steadiness.errorbar(offsets, steadiness_plot_data[10], yerr=yerr[10], fmt='o-') # bit 5
+    #ax_steadiness.errorbar(offsets, steadiness_plot_data[9], yerr=yerr[9], fmt='o-') # bit 6
+    #ax_steadiness.errorbar(offsets, steadiness_plot_data[15], fmt='rx-') # bit 0
 
     ax_uniqueness.plot(
         offsets, uniqueness_plot_data[0], 'x-',
-        offsets, uniqueness_plot_data[11], 'x-',
-        offsets, uniqueness_plot_data[10], 'x-',
-        offsets, uniqueness_plot_data[9], 'x-',
+        #offsets, uniqueness_plot_data[11], 'x-',
+        #offsets, uniqueness_plot_data[10], 'x-',
+        #offsets, uniqueness_plot_data[9], 'x-',
     )
 
     plt.show()
--- examples/teropuf_remote.py
+++ examples/teropuf_remote.py
@@ -6,10 +6,13 @@ import json
 from more_itertools import numeric_range
 from decimal import Decimal
 from ctypes import *
+from enum import Enum, auto
 
 from litex.tools.litex_client import RemoteClient
 from litescope.software.driver.analyzer import LiteScopeAnalyzerDriver
 from litescope.software.dump import DumpData, Dump
+
+from metastable import PUFType
 
 import argparse
 parser = argparse.ArgumentParser()
@@ -17,9 +20,9 @@ parser.add_argument("--identity", default=None)
 parser.add_argument('--analyzer', action='store_true')
 parser.add_argument('--voltage', action='store_true')
 parser.add_argument('--samples', type=int, default=100)
+parser.add_argument('--type', type=lambda t: PUFType[t], choices=list(PUFType))
 
 args = parser.parse_args()
-
 wb = RemoteClient(csr_csv="test/csr.csv")
 wb.open()
 
@@ -32,7 +35,6 @@ if args.analyzer:
     analyzer.configure_subsampler(subsampling)  ## increase this to "skip" cycles, e.g. subsample
     analyzer.configure_group(0)
 
-    # analyzer_samples = defaultdict(lambda: defaultdict(list))
 if args.voltage:
     from dwfconstants import *
 
@@ -54,7 +56,8 @@ if args.voltage:
     # master enable
     dwf.FDwfAnalogIOEnableSet(hdwf, c_int(True))
 
-    time.sleep(120)
+    if True:
+        time.sleep(120)
 
     def voltage_range(start, stop, step):
         # max recommended operating conditions for ECP5-5G is 1.26V
@@ -64,12 +67,11 @@ if args.voltage:
 
     voltage_iter = voltage_range(Decimal('1.1'), Decimal('1.31'), Decimal('0.02'))
     samples_iter = product(samples_iter, voltage_iter)
-    # voltage_samples = defaultdict(lambda: defaultdict(list))
 
 samples_iter = list(samples_iter)
-for s1, s2 in product(range(4), repeat=2):
+for s1, s2 in product(range(5), repeat=2):
     for sample_idx in samples_iter: # take n samples
-        sample = {'offset': None}
+        sample = {}
         if args.analyzer:
             analyzer.clear()
             analyzer.add_falling_edge_trigger("puf_reset")
@@ -83,12 +85,12 @@ for s1, s2 in product(range(4), repeat=2):
             dwf.FDwfAnalogIOChannelNodeSet(hdwf, c_int(0), c_int(1), c_double(voltage))
             time.sleep(0.3)
 
-        wb.regs.teropuf_reset.write(1) # enable reset
-        wb.regs.teropuf_cell0_select.write(s1)
-        wb.regs.teropuf_cell1_select.write(s2)
-        wb.regs.teropuf_reset.write(0) # disable reset
+        wb.regs.puf_reset.write(1) # enable reset
+        wb.regs.puf_cell0_select.write(s1)
+        wb.regs.puf_cell1_select.write(s2)
+        wb.regs.puf_reset.write(0) # disable reset
         time.sleep(0.1)
-        bit_value = wb.regs.teropuf_bit_value.read()
+        bit_value = wb.regs.puf_bit_value.read()
         sample['value'] = bit_value
         print(f'Comparator from set {s1} and {s2}: {c_int16(bit_value).value}')
 
@@ -99,22 +101,26 @@ for s1, s2 in product(range(4), repeat=2):
             analyzer.upload()
             analyzer_dump = Dump()
             analyzer_dump.add_from_layout(analyzer.layouts[analyzer.group], analyzer.data)
-            # dumps are not indexed
-            cv1 = next(v.values for v in  analyzer_dump.variables if v.name == 'teropuf_roset0_counter')
-            cv2 = next(v.values for v in  analyzer_dump.variables if v.name == 'teropuf_roset1_counter')
-            # each clock cycles has two values (rising, falling), skip every other value
-            counter_values = list(zip(cv1, cv2))[::2]
-            for offset, (counter1, counter2) in zip(count(0, subsampling), counter_values):
-                puf_response = counter1 - counter2
+
+            if args.type is PUFType.RO or args.type is PUFType.TERO:
+                cv1 = next(v.values for v in  analyzer_dump.variables if v.name == 'puf_roset0_counter')
+                cv2 = next(v.values for v in  analyzer_dump.variables if v.name == 'puf_roset1_counter')
+                # each clock cycles has two values (rising, falling), skip every other value
+                signal_values = list(zip(cv1, cv2))[::2]
+            elif args.type is PUFType.HYBRID:
+                signal_values = list(next(v.values for v in  analyzer_dump.variables if v.name == 'puf_ff_o'))
+            for offset, signal_value in zip(count(0, subsampling), signal_values):
+                if args.type is PUFType.RO or args.type is PUFType.TERO:
+                    counter1, counter2 = signal_value
+                    puf_response = counter1 - counter2
+                elif args.type is PUFType.HYBRID:
+                    puf_response = signal_value
                 # index responses by offset (in clock cycles)
                 sample = {
                     'offset': offset,
                     'value': puf_response
                 }
                 samples[f'{s1}:{s2}'].append(sample)
-                #analyzer_samples[offset][f'{s1}:{s2}'].append(puf_response)
-        # if args.voltage:
-        #     voltage_samples[voltage][f'{s1}:{s2}'].append(bit_value)
 
 if args.analyzer:
     analyzer.save("test/dump.vcd")
@@ -125,10 +131,8 @@ wb.close()
 
 dump = {
     'ident': args.identity,
-    'dump': samples,
-    # 'analyzer': analyzer_samples if args.analyzer else [],
-    # 'voltage': voltage_samples if args.voltage else [],
+    'dump': samples
 }
 
-with open(f'{args.identity or "teropuf"}_dump.json', 'w') as dumpfile:
+with open(f'{args.identity or "puf"}_dump.json', 'w') as dumpfile:
     json.dump(dump, dumpfile)
