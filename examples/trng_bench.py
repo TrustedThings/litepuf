9cb483186e87614b69914418285a65b33f5f302c
Arnaud Durand <arnaud.durand@unifr.ch>
2021-06-07 01:25:31 +0200

    Clean TRNG testbench

--- examples/trng_bench.py
+++ examples/trng_bench.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-from os import path
+import argparse
+import os
 from itertools import cycle, islice, chain, count
 
 from migen import *
@@ -13,7 +14,7 @@ from litex.build.generic_platform import Subsignal, IOStandard, Pins
 from litex_boards.platforms import ecp5_evn
 from litex_boards.targets.ecp5_evn import BaseSoC
 
-from litescope import LiteScopeIO, LiteScopeAnalyzer
+from litescope import LiteScopeAnalyzer
 
 from metastable import RingOscillator
 from metastable.oscillator import MetastableOscillator
@@ -38,7 +39,7 @@ class LiteScopeSoC(BaseSoC):
     }
     csr_map.update(BaseSoC.csr_map)
 
-    def __init__(self):
+    def __init__(self, num_osc=4, osc_len=7):
         sys_clk_freq = int(50e6) # check
 
         BaseSoC.__init__(self, sys_clk_freq, x5_clk_freq=int(50e6), toolchain="trellis", # check
@@ -54,55 +55,13 @@ class LiteScopeSoC(BaseSoC):
         self.submodules.bridge = bridge
         self.add_wb_master(bridge.wishbone)
 
-        self.platform.add_source_dir(path.join(path.dirname(__file__), 'verilog/lattice_ecp5'))
-        self.platform.add_source(path.join(path.dirname(__file__), 'verilog/lfsr.v'))
-        self.platform.add_source(path.join(path.dirname(__file__), 'verilog/random.v'))
-
-        metastable = Signal(name_override="metastable")
-        metastable.attr.add(("keep", 1))
-        rand_out = Signal(8, name_override="lfsr_weak")
-        
-        chain_out = Signal(name_override="chain_out")#, attr=set(["keep", ("noglobal", "1")]))
-        chain_out.attr.add(("keep", 1))
-        chain_out.attr.add(("noglobal", 1))
-        
-        self.submodules.ro = RingOscillator(
-            [
-                "X20/Y20/SLICEA",
-                "X20/Y20/SLICEB",
-                "X20/Y20/SLICEC",
-                "X20/Y20/SLICED",
-            ])
-
-        # Litescope IO
-        self.submodules.io = LiteScopeIO(8)
-        for i in range(7):#8
-            try:
-                self.comb += self.platform.request("user_led", i).eq(self.io.output[i])
-            except:
-                pass
+        oscillators = [RingOscillator(placement) for placement in ro_placer(num_osc, osc_len)]
+        self.submodules.trng = trng = RandomLFSR(oscillators)
 
         # Litescope Analyzer
         analyzer_groups = {}
 
-        self.specials += [
-        Instance("randomized_lfsr_weak",
-                    i_clk=self.crg.cd_sys.clk,
-                    i_rst=~self.platform.lookup_request("rst_n"),
-                    o_out=rand_out,
-                    o_metastable=metastable
-                    ),
-        ]
-
-        oscillators = [RingOscillator(placement) for placement in ro_placer(4, 5)]
-        self.submodules.trng = trng = RandomLFSR(oscillators)
-
         analyzer_groups[0] = [
-            rand_out,
-            metastable
-        ]
-
-        analyzer_groups[1] = [
             trng.word_ready,
             trng.reset,
             trng.word_o,
@@ -123,13 +82,22 @@ class LiteScopeSoC(BaseSoC):
             csr_csv="test/analyzer.csv")
 
 
-soc = LiteScopeSoC()
-builder = Builder(soc, csr_csv="test/csr.csv", csr_json="test/csr.json")
-vns = builder.build(nowidelut=True, ignoreloops=True)
+def main():
+    parser = argparse.ArgumentParser(description="TRNG testbench on ECP5 Evaluation Board")
+    parser.add_argument("--load",         action="store_true", help="Load bitstream")
+    parser.add_argument('--num-oscillators', type=int, default=4)
+    parser.add_argument('--oscillators-length', type=int, default=7)
+    args = parser.parse_args()
 
-#
-# Create csr and analyzer files
-#
-soc.finalize()
+    soc = LiteScopeSoC(
+        num_osc=args.num_oscillators,
+        osc_len=args.oscillators_length)
+    builder = Builder(soc, csr_csv="test/csr.csv", csr_json="test/csr.json")
+    vns = builder.build(nowidelut=True, ignoreloops=True)
 
-soc.do_exit(vns)
+    if args.load:
+        prog = soc.platform.create_programmer()
+        prog.load_bitstream(os.path.join(builder.gateware_dir, soc.build_name + ".svf"))
+
+if __name__ == "__main__":
+    main()
